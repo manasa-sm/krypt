b615d58b5f4007c4f0031752f36e43794e712cb5
Aryan0501 <84375923+Aryan0501@users.noreply.github.com>
2021-09-01 22:49:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 
 import React from 'react';
 
-//import RootStack from './navigators/RootStack'
+import RootStack from './navigators/RootStack'
 //import Signup from './screens/Signup';
 // need to remove when using stack 
-import Login from './screens/Login'; 
-import Signup from './screens/Signup';
+//import Login from './screens/Login'; 
+//import Signup from './screens/Signup';
 
 
 export default function App() {
-  return <Signup></Signup>;
+  return <RootStack/>;
 }
 
